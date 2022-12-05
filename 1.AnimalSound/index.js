7be50d854e91f7034335450bb365cb4caa18f093
yogesh16singh <ys1997642@gmail.com>
2022-12-05 22:06:21 +0530

    Added first project

---
+++ 1.AnimalSound/index.js
@@ -0,0 +1,6 @@
+let lion = new Audio("../sound/lion_1.mp3");
+let cat = new Audio("../sound/domestic_cat.mp3");
+let dog = new Audio("../sound/dogbark.wav");
+let elephant = new Audio("../sound/elephant.wav");
+let frog = new Audio("../sound/frog.mp3");
+
